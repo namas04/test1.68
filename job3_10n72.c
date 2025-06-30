a335c416540c76b6a037046ad766fd002c49bae3
Namas <jinnipa003@gmail.com>
2025-06-30 10:08:46 +0700

    main

---
+++ job3_10n72.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    float num,num2=0.0;
+    printf("Enter number : ");
+    scanf("%f",&num);
+    
+    num2 = num*2;
+    if ( num2==num*2);
+   printf("num2 = %.2f",num2);
+}
